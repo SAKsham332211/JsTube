813849109f1ec40519366ccfdbf21d2610147f99
saksham <sharma.saksham137@gmail.com>
2023-09-24 15:36:39 +0530

    conditional logged in

---
+++ 02Basics/06ConditionalLogin.js
@@ -0,0 +1,19 @@
+var email = true;
+var google = false;
+var facebook = false;
+
+// if (email) {
+//   console.log("email is verified");
+// }
+
+// if (google) {
+//   console.log("google is logged in");
+// }
+
+// if (facebook) {
+//   console.log("facebook logged in");
+// }
+
+if (email || google || facebook) {
+  console.log("logged in successs");
+}
