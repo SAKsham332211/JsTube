fb7d2e99ccdb502642fac7931e965755838a51d6
saksham <sharma.saksham137@gmail.com>
2023-09-29 14:34:52 +0530

    userRolefunction (switch using (`) mark)

---
+++ 03Intermediate/02userRoleFunction.js
@@ -0,0 +1,31 @@
+function getUserRole(name, role) {
+  switch (role) {
+    case "admin":
+      return `${name} is admin with all success `;
+      break;
+
+    case "subadmin":
+      return `${name} is subadmin with access to create/delete courses `;
+      break;
+
+    case "testprep":
+      return `${name} is testprep with access to create/delete tests `;
+      break;
+
+    case "user":
+      return `${name} is a user consume all content `;
+      break;
+
+    default:
+      return `${name} is a trail user `;
+      break;
+  }
+}
+
+getUserRole("saksham", "admin");
+
+var greetings = getUserRole("saksham", "admin");
+
+console.log(greetings);
+
+console.log(getUserRole("neha", "admin"));
