16fce9c59bf1ec2d0fe24997476839e00ca9a406
saksham <sharma.saksham137@gmail.com>
2023-09-29 13:46:47 +0530

    01Functions (1st Program)

---
+++ 03Intermediate/01functions.js
@@ -0,0 +1,18 @@
+// function sayHello(name) {
+//   console.log("Hello there, Hitesh");
+
+//   //   console.log("Hello there, ", name);
+
+//   console.log(`Hello there, ${name}, how are you?`);
+// }
+
+// sayHello("hitesh");
+// sayHello("sammy");
+
+function namastey() {
+  return "Hello India";
+}
+
+namastey();
+var greetings = namastey();
+console.log(greetings);
