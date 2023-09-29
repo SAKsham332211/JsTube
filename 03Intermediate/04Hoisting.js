253135fc46f77443d4a61143d89f971cb3ee614f
saksham <sharma.saksham137@gmail.com>
2023-09-29 17:37:34 +0530

    04Hoisting.js(tipper,bigtipper)

---
+++ 03Intermediate/04Hoisting.js
@@ -0,0 +1,25 @@
+// function tipper(a) {
+//   var bill = a;
+
+//   //   or we can also write as a // var bill = parseInt(a); will result the same answer.
+//   console.log(bill + 5);
+// }
+
+// tipper(80);
+
+// function bigtipper(a) {
+//   var bill = parseInt(a);
+//   console.log(bill + 15);
+// }
+
+// bigtipper(200);
+
+var name = "sharmaji";
+console.log(name);
+
+function sayName() {
+  var name = "Mr.Saksham Sharma";
+  console.log(name);
+}
+
+sayName();
