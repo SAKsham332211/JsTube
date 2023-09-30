939d6bb97582e7a5acc575b3bbca2bb975156d98
saksham <sharma.saksham137@gmail.com>
2023-09-30 17:05:11 +0530

    07Array.js (1st program)

---
+++ 03Intermediate/07Array.js
@@ -0,0 +1,30 @@
+var countries = ["INDIA", "PAKISTAN", "NEW-ZEALAND", "AMERICA", "RUSSIA"];
+
+var states = new Array("RAJASTHAN", "DELHI", "MUMBAI", "ASSAM", "BIHAR");
+
+console.log(states);
+console.log(countries);
+
+console.log(states[1]);
+console.log(states.length);
+states[0] = "UttarPradesh";
+
+console.log(states);
+
+var user = ["SAKSHAM SHARMA", "SHARMA.SAKSHAM137@GMAIL.COM", 3, 34, true];
+console.log(user);
+
+user.pop();
+user.pop();
+user.pop();
+console.log(user);
+
+user.unshift("NEHA SHARMA");
+console.log(user);
+
+user.shift();
+console.log(user);
+
+console.log(user.indexOf("SAKSHAM SHARMA"));
+
+console.log(Array.from("SAKSHAM SHARMA"));
