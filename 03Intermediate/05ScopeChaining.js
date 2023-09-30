1bb80dc6d5f9d0e4cac69a1b59ae5af7b5d1a22b
saksham <sharma.saksham137@gmail.com>
2023-09-30 15:21:11 +0530

    05ScopingChaining.js

---
+++ 03Intermediate/05ScopeChaining.js
@@ -0,0 +1,17 @@
+var name = "Saksham Sharma";
+
+console.log("line no 3", name);
+
+function sayName() {
+  var name = "neha";
+  console.log("line no 7", name);
+}
+
+sayName();
+
+function sayNameTwo() {
+  var name = "Ayush";
+  console.log("line no 14", name);
+}
+
+sayNameTwo();
