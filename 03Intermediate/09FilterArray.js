95ed4a705794a83a52b0a487b0820d13d8636381
saksham <sharma.saksham137@gmail.com>
2023-10-01 14:00:11 +0530

    09Array.js (splice & slice)

--- 03Intermediate/09FilterArray.js
+++ 03Intermediate/09FilterArray.js
@@ -1,9 +1,20 @@
-var testArray = [2, 3, 4, 5, 6, 7, 8, 9];
-console.log(testArray.fill("h", 0, 1));
-console.log(testArray.fill("h", 2, 3));
+//var testArray = [2, 3, 4, 5, 6, 7, 8, 9];
+//console.log(testArray.fill("h", 0, 1));
+//console.log(testArray.fill("h", 2, 3));
 
 // .........................................filer begins
 
-// var myNumber = [22, 33, 44, 55, 66, 77, 88, 99];
-// var result = myNumber.filter((num) => num != 44);
-// console.log(result);
+//var myNumber = [22, 33, 44, 55, 66, 77, 88, 99];
+//var result = myNumber.filter((num) => num != 44);
+//console.log(result);
+
+// ............................................slice begins
+
+var users = ["tim", "tam", "sam", "sim", "kon", "kan", "sar", "far"];
+
+//console.log(users.slice(1, 5));
+
+// ..............................................splice begins
+
+users.splice(1, 3, "hi");
+console.log(users);
