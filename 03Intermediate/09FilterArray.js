5924ce91b9f036617331a2ac8620dd022c6cd4e2
saksham <sharma.saksham137@gmail.com>
2023-10-01 13:19:45 +0530

    09FillFilterArray.js

---
+++ 03Intermediate/09FilterArray.js
@@ -0,0 +1,9 @@
+var testArray = [2, 3, 4, 5, 6, 7, 8, 9];
+console.log(testArray.fill("h", 0, 1));
+console.log(testArray.fill("h", 2, 3));
+
+// .........................................filer begins
+
+// var myNumber = [22, 33, 44, 55, 66, 77, 88, 99];
+// var result = myNumber.filter((num) => num != 44);
+// console.log(result);
